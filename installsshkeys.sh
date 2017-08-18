7d7f89c4ab543cfe7537a3f9a42f3b60f211cad1
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2017-08-18 11:31:24 +0200

    use a function

--- installsshkeys.sh
+++ installsshkeys.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
-echo -ne "Copying ssh config to your $HOME/.ssh/ ..."
-cp common/dotsshconfig/.ssh/config_zoodind ~/.ssh/
-if [[ $? -eq 0 ]]; then
-	echo -ne "[OK]\n"
-fi
-echo -ne "Copying ssh private key to your $HOME/.ssh/ ..."
-cp common/dotsshconfig/.ssh/id_rsa_zoodind ~/.ssh/
-if [[ $? -eq 0 ]]; then
-	echo -ne "[OK]\n"
-fi
-echo -ne "Copying ssh pub key to your $HOME/.ssh/ ..."
-cp common/dotsshconfig/.ssh/id_rsa_zoodind.pub ~/.ssh/
+
+function checkreturncode() {
 if [[ $? -eq 0 ]]; then
         echo -ne "[OK]\n"
 fi
+}
+
+echo -ne "Copying ssh config to your $HOME/.ssh/ ..."
+cp common/dotsshconfig/config_zoodind ~/.ssh/
+checkreturncode
+
+echo -ne "Copying ssh private key to your $HOME/.ssh/ ..."
+cp common/dotsshconfig/id_rsa_zoodind ~/.ssh/
+checkreturncode
+
+echo -ne "Copying ssh pub key to your $HOME/.ssh/ ..."
+cp common/dotsshconfig/id_rsa_zoodind.pub ~/.ssh/
+checkreturncode
