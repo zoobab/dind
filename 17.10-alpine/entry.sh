b94d3a16cbb143578277bdd8144886a364a95a49
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2017-12-15 17:36:23 +0100

    entry.sh: add a reponame env var

--- 17.10-alpine/entry.sh
+++ 17.10-alpine/entry.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 /usr/sbin/sshd &
 dockerd &
-# busybox sleep does not have infinity, so using that trick
-tail -f /dev/null
+
+DIR="/mnt"
+if [ -z "$REPONAME" ]; then
+    echo "REPONAME is not defined, sleeping forever..."
+    #busybox sleep does not have infinity, so using that trick
+    tail -f /dev/null
+else
+    #todo: test if there if a Dockerfile is present
+    echo "REPONAME is defined as $REPONAME, trying to build it..."
+    cd $DIR/$REPONAME
+    docker build .
+fi
